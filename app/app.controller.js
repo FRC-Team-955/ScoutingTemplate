a50b6b0094586ef266b5003805e8f73367613f9a
monochromicon <zaktan@outlook.com>
2015-04-28 00:01:02 -0700

    Added Angular v1.3.4 template plus Semantic UI

---
+++ app/app.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+  
+  angular
+    .module('app.controllers')
+    .controller('AppController', AppController);
+  
+  AppController.$inject = ['$rootScope'];
+  
+  function AppController ($rootScope) {
+    // controller implementation
+  }
+  
+})();
---
+++ app/app.routes.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+  
+  angular.module('app').config(
+    function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: '/app/home/home.html',
+          controller: 'HomeController',
+          controllerAs: 'vm'
+        })
+        .state('about', {
+          url: '/about',
+          templateUrl: '/app/about/about.html',
+          controller: 'AboutController',
+          controllerAs: 'vm'
+        })
+        .state('contact', {
+          url: '/contact',
+          templateUrl: '/app/contact/contact.html',
+          controller: 'ContactController',
+          controllerAs: 'vm'
+        })
+      ;
+      // default path to navigate (also used as a fallback for missing urls)
+      $urlRouterProvider.otherwise('/');
+    }
+  );
+  
+})();
