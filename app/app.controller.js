5528635208290b17354e842a20b7a7cb9b1eea0a
monochromicon <zaktan@outlook.com>
2015-05-01 14:10:01 -0700

    Navbar uses Angular to render

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,3 +1,34 @@
-app.controller("app.controller", ["$scope". function($scope) {
-	$scope.title = ""
+/* global app */
+app.controller("AppController",
+	["$scope", function($scope) {
+		$scope.menus = [
+			{
+				icon: "home icon",
+				label: "Home"
+			},
+			{
+				icon: "calendar icon",
+				label: "Schedule"
+			},
+			{
+				icon: "pie chart icon",
+				label: "Statistics"
+			},
+			{
+				icon: "sitemap icon",
+				label: "Alliance"
+			},
+			{
+				icon: "calculator icon",
+				label: "OPR Calc"
+			},
+			{
+				icon: "upload icon",
+				label: "Upload Data"
+			},
+			{
+				icon: "download icon",
+				label: "Download Data"
+			}
+		];
 }]);
